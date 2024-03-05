06d37583d6e066da60e1b8aee3efe6077da11160
squareface <squareface.officiel@gmail.com>
2024-03-05 20:39:04 +0100

    🩹 Correction de la méthode d'ajout de commandes de livres (fonctionnelle)

--- Controle.php
+++ Controle.php
@@ -76,13 +76,16 @@ class Controle{
      * @param array $champs nom et valeur des champs
      */
     public function post($table, $champs){
-        $result = $this->accessBDD->insertOne($table, $champs);
-        if (!$result){
-            $this->reponse(400, self::INVALID_REQUEST);
-        }else{
-            if ($table=="commandedocument"){
-                $result = $this->accessBDD->insertCommandeDocument($champs);
-            }
+        if ($table == 'commandedocument') {
+            $result = $this->accessBDD->insertCommandeDocument($champs);
+        } else {
+            $result = $this->accessBDD->insertOne($table, $champs);
+        }
+
+        if ($result == null || $result == false) {
+            $this->reponse(400, "requete invalide");
+        } else {
+            $this->reponse(200, "OK");
         }
     }
 
